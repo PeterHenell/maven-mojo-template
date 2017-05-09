a66b6cbadd933bc7684ede0d082b820135361d57
Henell, Peter <peter.henell@sek.se>
2017-05-09 14:44:57 +0200

    added parameter to mojo and setting parameter value in the calling project

--- cm-maven-plugin/src/main/java/com/peterhenell/se/cm/GreetingMojo.java
+++ cm-maven-plugin/src/main/java/com/peterhenell/se/cm/GreetingMojo.java
@@ -3,6 +3,7 @@ package sample.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
  
 /**
  * Says "Hi" to the user.
@@ -11,8 +12,14 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo( name = "sayhi")
 public class GreetingMojo extends AbstractMojo
 {
+    /**
+     * The greeting to display.
+     */
+    @Parameter( property = "sayhi.greeting", defaultValue = "Hello World!" )
+    private String greeting;
+
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+        getLog().info( greeting );
     }
 }
